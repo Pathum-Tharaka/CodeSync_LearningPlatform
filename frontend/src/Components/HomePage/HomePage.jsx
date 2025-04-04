e239a5c5f4ee2e364cec434723586fd592b4f36c
pathumtharaka <it22600516@my.sliit.lk>
2025-04-04 21:17:32 +0530

    Update HomePage.jsx

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
+import Navigation from '../Navigation/Navigation';
 
 const HomePage = () => {
     return (
       <Grid container xs={12} className="px-5 lg:px-36 justify-between">
-        <Grid item xs={0} lg={2.5} className="hidden lg:block w-full relative">
-        <p className='text-center'>Grid 1</p>
-          
+        <Grid item xs={0} lg={3} className="relative hidden lg:block">
+          <Navigation/>          
+        </Grid >
+        <Grid item xs={12} lg={6} className="relative hidden lg:block">
+        <p className='text-center'>Grid 2</p>
         </Grid>
-        <Grid item xs={12} lg={6} className="hidden lg:block w-full relative">
-        <p className='text-center'>Grid 1</p>
-        </Grid>
-        <Grid item xs={0} lg={3} className="hidden lg:block w-full relative">
-        <p className='text-center'>Grid 1</p>
+        <Grid item xs={0} lg={3} className="relative hidden lg:block">
+        <p className='text-center'>Grid 3</p>
         </Grid>
       </Grid>
     );
