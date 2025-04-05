3a543819e4de735a4cd8ac4070103844a2f7236d
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-05 23:58:26 +0530

    Enhance ProfileModal: add image upload functionality and improve styling

--- frontend/src/Components/Profile/ProfileModel.jsx
+++ frontend/src/Components/Profile/ProfileModel.jsx
@@ -3,9 +3,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import { IconButton } from "@mui/material";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 
 const style = {
   position: "absolute",
@@ -14,10 +14,11 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 600,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "none",
   boxShadow: 24,
   p: 4,
   outline: "none",
+  borderRadius: 4,
 };
 
 export default function ProfileModal() {
@@ -41,6 +42,17 @@ export default function ProfileModal() {
     onsubmit: handleSubmit,
   });
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        formik.setFieldValue("image", reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
@@ -53,13 +65,34 @@ export default function ProfileModal() {
         <Box sx={style}>
           <form onsubmit={formik.handleSubmit}>
             <div className="flex item-center justify-between">
-                <div className="flex items-center space-x-3">
-                   <IconButton onClick={handleClose} aria-label="delete">
-                        <CloseIcon/>
-                        <p>Edit Profile</p>
-                   </IconButton>
-                </div>
+              <div className="flex items-center space-x-3">
+                <IconButton onClick={handleClose} aria-label="delete">
+                  <CloseIcon />
+                </IconButton>
+                <p className="text-sm">Edit Profile</p>
+              </div>
+              <button type="submit" className="">
+                Save
+              </button>
+            </div>
+            <div className="overflow-y-scroll overflow-x-hidden h-[80vh]">
+              <div>
+                <div className="w-full">
+                  <div className="relative"></div>
+                       <img 
+                       className="w-full h-[12rem] object-cover object-center"  
+                       src="https://cdn.pixabay.com/photo/2023/07/25/19/47/milky-way-8149815_640.jpg" alt="cover" />
 
+                       <input
+                          type="file"
+                          className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                          name="backgroundImage"
+                          onChange={handleImageChange}
+                        />
+
+
+                </div>
+              </div>
             </div>
           </form>
         </Box>
