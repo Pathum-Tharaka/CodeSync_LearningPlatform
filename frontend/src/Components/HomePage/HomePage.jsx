982cda02df2a870fe083dcc57b7eb5bb80070f59
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-05 12:53:41 +0530

    Update routing in App and HomePage components to include dynamic profile route

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -8,16 +8,17 @@ import { Route, Routes } from "react-router-dom";
 const HomePage = () => {
   return (
     <Grid container xs={12} className="px-5 lg:px-36 justify-between">
-      <Grid item xs={0} lg={0} className="hidedn lg:block w-full relative">
+      <Grid item xs={0} lg={0} className="relative hidden lg:block">
         <Navigation />
       </Grid>
-      <Grid item xs={12} lg={6} className="px-5 lg:px-9 hidden w-full relative">
+      <Grid item xs={12} lg={6} className="relative hidden lg:block ">
         <Routes>
           <Route path="/" element={<HomeSection />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<Profile />} />
         </Routes>
       </Grid>
-      <Grid item xs={0} lg={3} className="relative lg:block w-full relative">
+      <Grid item xs={0} lg={3} className="relative hidden lg:block">
         <p className="text-center">Grid 3</p>
       </Grid>
     </Grid>
