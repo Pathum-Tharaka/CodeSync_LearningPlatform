558f39f5f3ce71c3756049e9dcbe98bcb2ee2094
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-05 13:37:50 +0530

    Update Profile component to include a background image and fix styling issues

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import { useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
+import React from "react";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
 const Profile = () => {
-    const navigate = useNavigate();
-    const handleBack = () => {
-        navigate(-1);
-    };
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
 
   return (
     <div>
-    <section className={'z-50- flex items-center sticky top-0 bg-opacity-95'}>
-        <KeyboardBackspaceIcon className='cursor-pointer' onclick={handleBack}/>
-        <h1 className='py-5 text-xl font-bold opacity-90 ml-5'>Profile</h1>
-    </section>
-    <section className='pl-6'>
-        <div className='flex justfy-between items-start mt-5 h-[5rem]'>
-            <Avatar alt='code with jahan' className='trasnform translate-y-24'
-            src='https://images.unsplash.com/photo-1633657921013-2e5f3f8a8b7d?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60'
+      <section className={"z-50- flex items-center sticky top-0 bg-opacity-95"}>
+        <KeyboardBackspaceIcon
+          className="cursor-pointer"
+          onclick={handleBack}
+        />
+        <h1 className="py-5 text-xl font-bold opacity-90 ml-5">Profile</h1>
+      </section>
+
+      <section>
+        <img
+          className="w-[100%] h-[15rem] object-cover"
+          src="https://cdn.pixabay.com/photo/2021/02/15/13/08/twitter-6017758_960_720.png"
+          alt=""
+        />
+      </section>
+
+      <section className="pl-6">
+        <div className="flex justfy-between items-start mt-5 h-[5rem]">
+          <Avatar
+            alt="code with jahan"
+            className="trasnform -translate-y-24"
+            src="https://avatars.githubusercontent.com/u/183508853?s=200&v=4"
             sx={{ width: "10rem", height: "10rem", border: "4px solid white" }}
-            />
+          />
         </div>
-    </section>
-      
+      </section>
     </div>
-  )
-}
-export default Profile
+  );
+};
+export default Profile;
