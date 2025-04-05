cdf58970f65df001d4e6e3a30e2863630e24d8ca
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-05 12:37:16 +0530

    Add routing for HomeSection and Profile components in HomePage

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import Navigation from '../Navigation/Navigation';
-import HomeSection from '../HomeSection/HomeSection';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Navigation from "../Navigation/Navigation";
+import HomeSection from "../HomeSection/HomeSection";
+import Profile from "../Profile/Profile";
+import { Route, Routes } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-      <Grid container xs={12} className="px-5 lg:px-36 justify-between">
-        <Grid item xs={0} lg={0} className="relative hidden lg:block">
-          <Navigation/>
-        </Grid >
-        <Grid item xs={12} lg={6} className="relative hidden lg:block ">
-          <HomeSection/>
-        </Grid>
-        <Grid item xs={0} lg={3} className="relative hidden lg:block">
-        <p className='text-center'>Grid 3</p>
-        </Grid>
+  return (
+    <Grid container xs={12} className="px-5 lg:px-36 justify-between">
+      <Grid item xs={0} lg={0} className="hidedn lg:block w-full relative">
+        <Navigation />
       </Grid>
-    );
-  }
-  
-export default HomePage
+      <Grid item xs={12} lg={6} className="px-5 lg:px-9 hidden w-full relative">
+        <Routes>
+          <Route path="/" element={<HomeSection />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Grid>
+      <Grid item xs={0} lg={3} className="relative lg:block w-full relative">
+        <p className="text-center">Grid 3</p>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HomePage;
