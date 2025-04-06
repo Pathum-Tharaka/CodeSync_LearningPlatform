79f340b1378943442ac97dd7a07a53d3460bf9d0
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-06 23:51:32 +0530

    Remove codesync module and associated files

--- codesync/src/main/java/com/codesync/codesync/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codesync.codesync.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codesync.codesync.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
