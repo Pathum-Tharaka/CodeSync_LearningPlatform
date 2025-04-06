c9aa6215b4d9c08a7432eeb33536f3c18f01fbd7
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-06 23:54:23 +0530

    Initialize codesync module with Gradle configuration and basic application structure

---
+++ codesync/src/main/java/com/wizards/codesync/CodesyncApplication.java
@@ -0,0 +1,13 @@
+package com.wizards.codesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesyncApplication.class, args);
+	}
+
+}
