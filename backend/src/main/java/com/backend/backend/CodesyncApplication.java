e60468c1c1c26ccef38f0e96a9b2c6a636ba37fc
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-10 15:55:57 +0530

    Refactor backend: replace BackendApplication with CodesyncApplication as the main entry point

--- backend/src/main/java/com/backend/backend/CodesyncApplication.java
+++ backend/src/main/java/com/backend/backend/CodesyncApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class CodesyncApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CodesyncApplication.class, args);
 	}
 
 }
