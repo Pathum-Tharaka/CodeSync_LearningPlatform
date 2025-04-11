6379600e3645fa0dadc400175645275f501caf9d
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-11 08:09:37 +0530

    Remove unused constructor and serialVersionUID from UserException class

--- backend/src/main/java/com/backend/exception/UserException.java
+++ backend/src/main/java/com/backend/exception/UserException.java
@@ -1,14 +1,9 @@
 package com.backend.exception;
 
 public class UserException extends Exception {
-    private static final long serialVersionUID = 1L;
 
     public UserException(String message) {
         super(message);
     }
 
-    public UserException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    
 }
