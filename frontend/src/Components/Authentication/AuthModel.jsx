baf392e895c2eee10867126223663163968efd3e
JAHAN RAMESH <jahanrazh@gmail.com>
2025-04-13 11:54:44 +0530

    Refactor AuthModal component to include SignupForm and improve layout

--- frontend/src/Components/Authentication/AuthModel.jsx
+++ frontend/src/Components/Authentication/AuthModel.jsx
@@ -1,28 +1,29 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SigninForm from './SigninFrom';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SigninForm from "./SigninFrom";
+import SignupForm from "./SignupForm";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 550,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  borderRadius:2,
-  outline:"none"
+  borderRadius: 2,
+  outline: "none",
 };
 
-export default function AuthModal({open,handleClose}) {
+export default function AuthModal({ open, handleClose }) {
+  const location = useLocation();
   
 
   return (
     <div>
-      
       <Modal
         open={open}
         onClose={handleClose}
@@ -30,9 +31,24 @@ export default function AuthModal({open,handleClose}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-        <div>
-            <SigninForm/>
-        </div>
+          <h1 className="text-center font-bold text-2xl">
+            Create Your Account
+          </h1>
+          {location.pathname === "/signup" ? <SignupForm /> : <SigninForm />}
+
+          <h1 className="text-center py-5 font-semibold text-lg text-gray-500">
+            {location.pathname === "/signup"
+              ? "Already have an account?"
+              : "If you don't have an account"}
+          </h1>
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={handleNavigate}
+            sx={{ borderRadius: "29px", py: "15px" }}
+          >
+            {location.pathname === "/signup" ? "Signin" : "Signup"}
+          </Button>
         </Box>
       </Modal>
     </div>
