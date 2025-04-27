596844b697f1e542e3a92cbe2466340403b06ebb
pathumtharaka <it22600516@my.sliit.lk>
2025-04-27 17:13:18 +0530

    Create SecurityContest.java

---
+++ backend/src/main/java/com/backend/config/SecurityContest.java
@@ -0,0 +1,8 @@
+package com.backend.config;
+
+public class SecurityContest {
+	
+	public static final String JWT_KEY="zjdbfagvqruuyzbhgdfpeoajnebxiueban";
+	public static final String HEADER="Authorization";
+
+}
