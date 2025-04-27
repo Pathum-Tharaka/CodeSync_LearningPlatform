515b9db8d954987b7f092f523ffbe46fe54dc39e
pathumtharaka <it22600516@my.sliit.lk>
2025-04-27 17:13:46 +0530

    exception

---
+++ backend/src/main/java/com/backend/exception/NotificationException.java
@@ -0,0 +1,13 @@
+package com.backend.exception;
+
+public class NotificationException extends Exception {
+
+    public NotificationException() {
+        super();
+    }
+
+    public NotificationException(String message) {
+        super(message);
+    }
+}
+
---
+++ backend/src/main/java/com/backend/exception/StoryException.java
@@ -0,0 +1,14 @@
+package com.backend.exception;
+
+import com.backend.model.Story;
+
+public class StoryException extends Exception{
+
+	public StoryException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StoryException(String message) {
+		super();
+	}
+}
