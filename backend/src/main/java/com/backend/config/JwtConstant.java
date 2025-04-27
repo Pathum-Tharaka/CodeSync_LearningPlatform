718c8126faced8ed2bdee33e4d8813ada8053c7e
pathumtharaka <it22600516@my.sliit.lk>
2025-04-27 17:10:38 +0530

    deleted files for errors

--- backend/src/main/java/com/backend/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.backend.config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY = "1ea02665e01bda657797abe2ff565ba1296113e815de4ef7be6e2546b3777b736ff077620000d942d3bca577bcac483efe20d840ea9776488f9187656e1837f7";
-    public static final String JWT_HEADER = "Authorization";
-}
