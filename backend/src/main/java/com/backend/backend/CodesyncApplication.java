6d77cefa5dc39e1655d051569641d75b85813353
pathumtharaka <it22600516@my.sliit.lk>
2025-04-27 17:11:31 +0530

    upgraded

--- backend/src/main/java/com/backend/backend/CodesyncApplication.java
+++ backend/src/main/java/com/backend/backend/CodesyncApplication.java
@@ -2,10 +2,8 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.backend.model")
 public class CodesyncApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/backend/repository/UserRepository.java
+++ backend/src/main/java/com/backend/repository/UserRepository.java
@@ -1,17 +1,26 @@
 package com.backend.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.backend.exception.UserException;
+import com.backend.model.Post;
 import com.backend.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-	public User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer>{
 
-	@Query("SELECT DISTINCT u FROM User u WHERE u.fullName LIKE %:query% OR u.email LIKE %:query%")
-	public List<User> searchUser(@Param("query") String query);
-
-
+	public Optional<User> findByEmail(String email);
+	
+	public Optional<User> findByUsername(String username);
+	
+	@Query("SELECT u FROM User u WHERE u.id IN :users")
+    public List<User> findAllUserByUserIds(@Param("users") List<Integer> userIds);
+	
+	@Query("SELECT DISTINCT u FROM User u WHERE u.username LIKE %:query% OR u.email LIKE %:query%")
+	public List<User> findByQuery(@Param("query") String query);
+	
 }
