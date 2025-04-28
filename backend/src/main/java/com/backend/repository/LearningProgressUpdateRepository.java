110c3b5e8916b35a6d894513a84dbc4fae0a23d5
IT22600448 <128448910+Reshmi516@users.noreply.github.com>
2025-04-28 21:11:15 +0530

    Add method to find LearningProgressUpdates by user in LearningProgressUpdateRepository

--- backend/src/main/java/com/backend/repository/LearningProgressUpdateRepository.java
+++ backend/src/main/java/com/backend/repository/LearningProgressUpdateRepository.java
@@ -6,3 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+public interface LearningProgressUpdateRepository extends JpaRepository<LearningProgressUpdate, Long> {
+    List<LearningProgressUpdate> findByUser(User user);
+}
