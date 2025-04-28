fd94dd38bef99534170bbbd73cf2d1cbbbabccef
IT22600448 <128448910+Reshmi516@users.noreply.github.com>
2025-04-28 21:12:42 +0530

    Refactor LearningProgressUpdateService interface to improve method organization

--- backend/src/main/java/com/backend/services/LearningProgressUpdateService.java
+++ backend/src/main/java/com/backend/services/LearningProgressUpdateService.java
@@ -5,3 +5,13 @@ import com.backend.model.LearningProgressUpdate;
 
 import java.util.List;
 
+public interface LearningProgressUpdateService {
+
+    LearningProgressUpdate createProgressUpdate(LearningProgressUpdate update, Integer userId) throws UserException;
+
+    LearningProgressUpdate updateProgressUpdate(Long id, LearningProgressUpdate update, Integer userId) throws UserException;
+
+    void deleteProgressUpdate(Long id, Integer userId) throws UserException;
+
+    List<LearningProgressUpdate> getUserProgressUpdates(Integer userId) throws UserException;
+}
