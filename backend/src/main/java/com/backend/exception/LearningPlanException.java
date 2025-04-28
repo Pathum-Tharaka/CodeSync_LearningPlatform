3ebe60238a8794a217250e1ea9f97d4a771607a9
Senera Fernando <senerafernando631@gmail.com>
2025-04-28 22:14:48 +0530

    exception for learning plan

--- backend/src/main/java/com/backend/exception/LearningPlanException.java
+++ backend/src/main/java/com/backend/exception/LearningPlanException.java
@@ -1,5 +1,5 @@
 package com.backend.exception;
-
+//exception for learning plan
 public class LearningPlanException extends Exception {
     public LearningPlanException(String message) {
         super(message);
