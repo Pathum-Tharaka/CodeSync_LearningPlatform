fac3dd6fdabf61c2d96b2f1d335c3ef08d18f28a
Senera Fernando <senerafernando631@gmail.com>
2025-04-28 22:04:16 +0530

    lplanservices add

--- backend/src/main/java/com/backend/services/LearningPlanService.java
+++ backend/src/main/java/com/backend/services/LearningPlanService.java
@@ -9,19 +9,3 @@ import com.backend.model.Resource;
 import com.backend.model.Topic;
 
 import java.util.List;
-
-public interface LearningPlanService {
-    LearningPlan createLearningPlan(LearningPlan learningPlan, Integer userId) throws UserException;
-    LearningPlan updateLearningPlan(LearningPlan learningPlan, Integer userId) throws UserException, LearningPlanException;
-    LearningPlan getLearningPlanById(Long planId) throws LearningPlanException;
-    List<LearningPlan> getLearningPlansByUserId(Integer userId) throws UserException;
-    void deleteLearningPlan(Long planId, Integer userId) throws LearningPlanException, UserException;
-
-    Topic addTopicToPlan(Long planId, Topic topic, Integer userId) throws LearningPlanException, UserException;
-    Topic updateTopic(Long topicId, Topic topic, Integer userId) throws TopicException, UserException;
-    void deleteTopic(Long topicId, Integer userId) throws TopicException, UserException;
-
-    Resource addResourceToTopic(Long topicId, Resource resource, Integer userId) throws TopicException, UserException;
-    Resource updateResource(Long resourceId, Resource resource, Integer userId) throws ResourceException, UserException;
-    void deleteResource(Long resourceId, Integer userId) throws ResourceException, UserException;
-}
