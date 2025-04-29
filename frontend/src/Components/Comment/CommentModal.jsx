bb4929f0404f2b275f36a341febe2500081d3b75
pathumtharaka <it22600516@my.sliit.lk>
2025-04-29 21:56:22 +0530

    Update CommentModal.jsx

--- frontend/src/Components/Comment/CommentModal.jsx
+++ frontend/src/Components/Comment/CommentModal.jsx
@@ -26,7 +26,6 @@ import { findPostByIdAction } from "../../Redux/Post/Action";
 import CommentCard from "./CommentCard";
 import "./CommentModal.css";
 
-
 const CommentModal = ({
   isOpen,
   onClose,
@@ -98,8 +97,8 @@ const CommentModal = ({
               <div className="w-[45%] flex flex-col justify-center">
                 <img
                   className="max-h-full max-w-full"
-                  src={post?.singlePost?.image}
-                  alt="upload photo"
+                  src={post.singlePost?.image}
+                  alt="Upload photo"
                 />
               </div>
               <div className="w-[55%] pl-10 relative">
