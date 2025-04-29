958cc7b4e7b40485bd8aedd22bdb33ffb45d66e6
IT22600448 <128448910+Reshmi516@users.noreply.github.com>
2025-04-29 21:25:38 +0530

    Add LearningProgress route to Routers.jsx

--- frontend/src/Pages/Router/Routers.jsx
+++ frontend/src/Pages/Router/Routers.jsx
@@ -53,7 +53,7 @@ const Routers = () => {
           <Route path="/notifications" element={<Notification />} />
           <Route path="/create-story" element={<CreateStory />} />
           <Route path="/learning_plan" element={<LearningPlan />} />
-      
+          <Route path="/learning-progress" element={<LearningProgress />} />
           <Route path="/oauth-success" element={<OAuthSuccess />} />
         </Routes>
       </div>
